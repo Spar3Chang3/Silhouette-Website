07ce17b7154ad80ca7eb025d7f83175cb42238e4
Spar3Chang3 <baker.daryk@ic.edu>
2024-03-09 18:23:18 -0600

    Get hover function working - need to fix border

--- desktop/meet-the-band/src/App.jsx
+++ desktop/meet-the-band/src/App.jsx
@@ -1,49 +1,89 @@
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 export default function App() {
     const imgLink = "http://localhost:3000/";
 
+    const [ colinIsHovered, setColinIsHovered ] = useState(false);
     const colinImgStyle = {
-        backgroundImage: `url('${imgLink + "full-band.jpg"}')`
+        backgroundImage: `url('${imgLink + "colinPortrait.jpg"}')`
     }
+
+    const [ jackIsHovered, setJackIsHovered ] = useState(false);
+    const jackImgStyle = {
+
+    }
+
+    const [ simonIsHovered, setSimonIsHovered ] = useState(false);
+    const simonImgStyle = {
+
+    }
+
+    const [ noahIsHovered, setNoahIsHovered ] = useState(false);
+    const noahImgStyle = {
+
+    }
+
+    const [ henryIsHovered, setHenryIsHovered ] = useState(false);
+    const henryImgStyle = {
+        backgroundImage: `url('${imgLink + "henryPortrait.jpg"}')`
+    }
+
+
 
     return (
       <div className={"bandCardContainer"}>
           <div className={"colinCard"}>
-              <div className={"backgroundImg"}>
-              </div>
-              <div className={"bio"}>
+              <div className={"backgroundImg"} style={colinImgStyle}
+                   onMouseEnter={() => setColinIsHovered(true)}
+                   onMouseLeave={() => setColinIsHovered(false)}
+              ></div>
+              <div className={`bio ${colinIsHovered ? 'fadeIn' : ''}`}>
                   <h2>Colin Curry</h2>
                   <p></p>
               </div>
+              <div className={"socials"}>
+
+              </div>
           </div>
           <div className={"jackCard"}>
-              <div className={"backgroundImg"}></div>
-              <div className={"bio"}>
+              <div className={"backgroundImg"} style={colinImgStyle}
+                   onMouseEnter={() => setJackIsHovered(true)}
+                   onMouseLeave={() => setJackIsHovered(false)}
+              ></div>
+              <div className={`bio ${jackIsHovered ? 'fadeIn' : ''}`}>
                   <h2>Jack Riley</h2>
                   <p></p>
               </div>
           </div>
           <div className={"noahCard"}>
-              <div className={"backgroundImg"}></div>
-              <div className={"bio"}>
+              <div className={"backgroundImg"} style={colinImgStyle}
+                   onMouseEnter={() => setNoahIsHovered(true)}
+                   onMouseLeave={() => setNoahIsHovered(false)}
+              ></div>
+              <div className={`bio ${noahIsHovered ? 'fadeIn' : ''}`}>
                   <h2>Noah Huddleston</h2>
                   <p></p>
               </div>
           </div>
           <div className={"simonCard"}>
-              <div className={"backgroundImg"}></div>
-              <div className={"bio"}>
+              <div className={"backgroundImg"} style={colinImgStyle}
+                   onMouseEnter={() => setSimonIsHovered(true)}
+                   onMouseLeave={() => setSimonIsHovered(false)}
+              ></div>
+              <div className={`bio ${simonIsHovered ? 'fadeIn' : ''}`}>
                   <h2>Simon Lanter</h2>
                   <p></p>
               </div>
           </div>
           <div className={"henryCard"}>
-              <div className={"backgroundImg"}></div>
-              <div className={"bio"}>
+              <div className={"backgroundImg"} style={henryImgStyle}
+                   onMouseEnter={() => setHenryIsHovered(true)}
+                   onMouseLeave={() => setHenryIsHovered(false)}
+              ></div>
+              <div className={`bio ${henryIsHovered ? 'fadeIn' : ''}`}>
                   <h2>Henry Shaffer</h2>
-                  <p></p>
+                  <p>Henry Schafer is the bassist, co-founder and co-lyricist for Silhouette. He first picked up the bass in the early years of high school, soon harbouring a fascination with the music of yesteryear and developing an eclectic playing style that would lead to his work in Silhouette. When not playing in the band his other pursuits include photography, silversmithing and lost media.</p>
               </div>
           </div>
       </div>

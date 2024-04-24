676d8c2ebae151ca576e8736362ac3ecbf4389a5
spar3chang3 <baker.daryk@ic.edu>
2024-04-24 00:01:15 -0500

    Fuck it, I'm building my own calendar now

--- desktop/event-schedule/src/components/EventCalendar.jsx
+++ desktop/event-schedule/src/components/EventCalendar.jsx
@@ -1,64 +1,42 @@
 import './EventCalendar.css';
+import {useState, useEffect} from "react";
 
 export default function EventCalendar() {
 
-    const lastDayStyle = {
-        visibility: 'hidden',
-        pointerEvents: 'none'
-    }
-
-    const lastMonthStyle = {
-        opacity: '50%'
-    }
-
-    const nextMonthStyle = {
-        opacity: '50%'
-    }
+    const [selectedEvent, setSelectedEvent] = useState(null);
 
     return (
         <>
             <div className={"eventCalendarContainer"}>
-                <div className={"calendarToolbar"}>
-                    <span>April, 2024</span>
-                </div>
                 <div className={"dayContainer"}>
-                    <div className={"day"} style={lastMonthStyle}>30</div>
-                    <div className={"day"} style={lastMonthStyle}>31</div>
-                    <div className={"day"}>1</div>
-                    <div className={"day"}>2</div>
-                    <div className={"day"}>3</div>
-                    <div className={"day"}>4</div>
-                    <div className={"day"}>5</div>
-                    <div className={"day"}>6</div>
-                    <div className={"day"}>7</div>
-                    <div className={"day"}>8</div>
-                    <div className={"day"}>9</div>
-                    <div className={"day"}>10</div>
-                    <div className={"day"}>11</div>
-                    <div className={"day"}>12</div>
-                    <div className={"day"}>13</div>
-                    <div className={"day"}>14</div>
-                    <div className={"day"}>15</div>
-                    <div className={"day"}>16</div>
-                    <div className={"day"}>17</div>
-                    <div className={"day"}>18</div>
-                    <div className={"day"}>19</div>
-                    <div className={"day"}>20</div>
-                    <div className={"day"}>21</div>
-                    <div className={"day"}>22</div>
-                    <div className={"day"}>23</div>
-                    <div className={"day"}>24</div>
-                    <div className={"day"}>25</div>
-                    <div className={"day"}>26</div>
-                    <div className={"day"}>27</div>
-                    <div className={"day"}>28</div>
-                    <div className={"day"}>29</div>
-                    <div className={"day"}>30</div>
-                    <div className={"day"}>31</div>
-                    <div className={"day"} style={nextMonthStyle}>1</div>
-                    <div className={"day"} style={nextMonthStyle}>2</div>
+                    <Event eventName={"Test"}/>
+                </div>
+                <div className={"descriptionContainer"}>
+                    <DisplayEvent/>
                 </div>
             </div>
         </>
     )
+
+
+    function Event(eventName, eventLocation, eventLocationLink, startTime, endTime, date, entryPrice, age) {
+        return (
+            <button className={"day"}
+                    onClick={setSelectedEvent("hi!")}
+            >{date}</button>
+        )
+    }
+
+    function DisplayEvent() {
+        return (
+            <div className={"description"}>
+                <h2>{selectedEvent.eventName}</h2>
+                <span>Location: <a href={selectedEvent.eventLocationLink}>{selectedEvent.eventLocationName}</a></span>
+                <span>Start Time: {selectedEvent.startTime}</span>
+                <span>End Time: {selectedEvent.endTime}</span>
+                <span>Entry Price: {selectedEvent.entryPrice}</span>
+                <span>Required Age: {selectedEvent.age}</span>
+            </div>
+        )
+    }
 }

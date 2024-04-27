101f02c0be2200bfb7206fcaa73066d2c4fddc54
spar3chang3 <baker.daryk@ic.edu>
2024-04-26 21:34:27 -0500

    Update Redirect.cjs to actually do it's fucking job

--- RedirectController/Redirect.cjs
+++ RedirectController/Redirect.cjs
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const https = require('https');
-const isMobile = require('is-mobile');
+const DeviceDetector = require('device-detector-js');
 
 const hostname = "silhouette.band";
+
+function isMobile (userAgent) {
+    const detector = new DeviceDetector();
+    const userDevice = detector.parse(userAgent);
+    return userDevice.device.type === 'smartphone' || userDevice.device.type === 'tablet';
+}
 
 app.get('/', (req, res) => {
     isMobile(req.headers['user-agent']) ? res.redirect("https://mobile.silhouette.band/") : res.redirect("https://desktop.silhouette.band/");
